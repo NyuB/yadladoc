012886b920879c5f5b5af52f17ac5e4f1e4ee9a0
NyuB <brice.decaestecker@gmx.fr>
2024-04-20 09:22:40 +0200

    Prepare tests for multiple snippets

--- yadladoc/test/src/nyub/yadladoc/YadladocSuite.scala
+++ yadladoc/test/src/nyub/yadladoc/YadladocSuite.scala
@@ -7,7 +7,7 @@ class YadladocSuite
     with AssertExtensions
     with SuiteExtensions:
 
-    withTempDir.test("One snippet"): (tmpDir: Path) =>
+    withTempDir.test("One snippet"): tmpDir =>
         val markdownFile = makeFile(tmpDir, "README.md"):
             l"""
             # One snippet
@@ -25,8 +25,49 @@ class YadladocSuite
         Yadladoc(Yadladoc.Settings(tmpDir, templateFile)).run(markdownFile)
 
         tmpDir.resolve("yadladoc.txt") hasContent l"""
-        package com.example
-        class HelloYadladoc { }
-        """
+            package com.example
+            class HelloYadladoc { }
+            """
+
+    withTempDir.test("Three snippets concatenated"): (tmpDir: Path) =>
+        val markdownFile = makeFile(tmpDir, "README.md"):
+            l"""
+            Create a list with listOf(...)
+            ```kotlin
+            val myList = listOf(1, 2, 3)
+            ```
+            Retrieve items with get(...)
+            ```kotlin
+            myList.get(0) // 1
+            myList[0] // operator alternative to get()
+            ```
+            Check if an object is in the list with contains(...)
+            ```kotlin
+            myList.contains(2) // true
+            myList.contains(42) // false
+            42 in myList // operator alternative to contains
+            ```
+            """
+        val templateFile = makeFile(tmpDir, "template.kt"):
+            l"""
+            package com.example
+            fun main() {
+            $${{ydoc.snippet}}
+            }
+            """
+        
+        Yadladoc(Yadladoc.Settings(tmpDir, templateFile)).run(markdownFile)
+
+        tmpDir.resolve("yadladoc.txt") hasContent l"""
+            package com.example
+            fun main() {
+            val myList = listOf(1, 2, 3)
+            myList.get(0) // 1
+            myList[0] // operator alternative to get()
+            myList.contains(2) // true
+            myList.contains(42) // false
+            42 in myList // operator alternative to contains
+            }
+            """
 
 end YadladocSuite
