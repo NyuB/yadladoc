54f7ce8f47511173e41823831f19079a2e7b28d3
NyuB <brice.decaestecker@gmx.fr>
2024-04-24 05:53:09 +0200

    Extract Example class from Yadladoc

---
+++ yadladoc/src/nyub/yadladoc/Example.scala
@@ -0,0 +1,24 @@
+package nyub.yadladoc
+
+case class Example(
+    val name: String,
+    val language: Option[String],
+    val content: Iterable[ExampleContent]
+):
+    def merge(other: Example): Example =
+        if other.language != language then
+            throw IllegalArgumentException(
+              s"Error trying to merge snippets with different languages ${language} and ${other.language}"
+            )
+        else
+            Example(
+              name,
+              language,
+              content ++ other.content
+            )
+
+case class ExampleContent(
+    prefixTemplateNames: Iterable[String],
+    body: Iterable[String],
+    suffixTemplateNames: Iterable[String]
+)
--- yadladoc/src/nyub/yadladoc/Yadladoc.scala
+++ yadladoc/src/nyub/yadladoc/Yadladoc.scala
@@ -157,29 +157,6 @@ object Yadladoc:
                     lines.foldLeft(Properties.empty): (props, line) =>
                         props.extendedWith(Properties.ofLine(line))
 
-private case class Example(
-    val name: String,
-    val language: Option[String],
-    val content: Iterable[ExampleContent]
-):
-    def merge(other: Example): Example =
-        if other.language != language then
-            throw IllegalArgumentException(
-              s"Error trying to merge snippets with different languages ${language} and ${other.language}"
-            )
-        else
-            Example(
-              name,
-              language,
-              content ++ other.content
-            )
-
-private case class ExampleContent(
-    prefixTemplateNames: Iterable[String],
-    body: Iterable[String],
-    suffixTemplateNames: Iterable[String]
-)
-
 private class SnippetMerger(
     val config: Yadladoc.Configuration,
     val examples: Map[String, Example]
