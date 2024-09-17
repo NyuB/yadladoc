8f2a734e3e4c8ccbd6e52d6d6b7cd2403b5b7174
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-17 10:05:39 +0200

    Remove now redundant tests

--- yadladoc/test/src/nyub/yadladoc/YadladocSuite.scala
+++ yadladoc/test/src/nyub/yadladoc/YadladocSuite.scala
@@ -11,95 +11,49 @@ class YadladocSuite
     with AssertExtensions
     with SuiteExtensions:
 
-    testWithinYDocContext("One snippet"): (outputDir, configDir, workingDir) =>
-        val markdownFile = makeFile(workingDir, "README.md"):
-            l"""
+    testWithinYDocContext("Generate one file from one snippet"):
+        (outputDir, configDir, workingDir) =>
+            val markdownFile = makeFile(workingDir, "README.md"):
+                l"""
             # One snippet
             ```java ydoc.example=one.java
             class HelloYadladoc { }
             ```
             """
 
-        Yadladoc(testConfig(configDir))
-            .run(outputDir, markdownFile)
+            Yadladoc(testConfig(configDir))
+                .run(outputDir, markdownFile)
 
-        outputDir.resolve("one.java") `has content` l"""
+            outputDir.resolve("one.java") `has content` l"""
             package com.example;
             class HelloYadladoc { }
             """
 
-    testWithinYDocContext("Three snippets, first two in the same example"):
-        (outputDir: Path, configDir: Path, workingDir) =>
-            val markdownFile = makeFile(workingDir, "README.md"):
-                l"""
-            Create a list with listOf(...)
-            ```kotlin ydoc.example=kotlin-list-example.kt
-                val myList = listOf(1, 2, 3)
-            ```
-            Retrieve items with get(...)
-            ```kotlin ydoc.example=kotlin-list-example.kt
-                myList.get(0) // 1
-                myList[0] // operator alternative to get()
-            ```
-            Here is how to define a class:
-            ```kotlin ydoc.example=kotlin-class-example.kt
-                class SoCool(val coolnessLevel: Int)
-            ```
-            """
-
-            val Results(generatedFiles, errors) =
-                Yadladoc(testConfig(configDir))
-                    .run(outputDir, markdownFile)
-
-            errors `is equal to` List.empty
-
-            generatedFiles `is equal to` List(
-              GeneratedFile(
-                Some(outputDir),
-                p"kotlin-list-example.kt",
-                markdownFile
-              ),
-              GeneratedFile(
-                Some(outputDir),
-                p"kotlin-class-example.kt",
-                markdownFile
-              )
-            )
-
-            outputDir.resolve("kotlin-list-example.kt") `has content` l"""
-            package com.example
-            fun main() {
-                val myList = listOf(1, 2, 3)
-                myList.get(0) // 1
-                myList[0] // operator alternative to get()
-            }
-            """
-
-            outputDir.resolve("kotlin-class-example.kt") `has content` l"""
-            package com.example
-            fun main() {
-                class SoCool(val coolnessLevel: Int)
-            }
-            """
-
-    testWithinYDocContext("Prefix and suffix inclusion"):
+    testWithinYDocContext("Generate files from multiple snippets"):
         (outputDir, configDir, workingDir) =>
             val markdownFile = makeFile(workingDir, "README.md"):
                 l"""
-            Here is how to use an awesome library:
-            ```java ydoc.example=surround.java ydoc.prefix=import-prefix ydoc.suffix=footnote-suffix
-            new AwesomeClass().doAmazingStuff(); // Wunderbar !
+            # One snippet
+            ```java ydoc.example=one.java
+            class One { }
+            ```
+            # Another snippet
+            ```java ydoc.example=two.java
+            class Two { }
             ```
             """
+
             Yadladoc(testConfig(configDir))
                 .run(outputDir, markdownFile)
 
-            outputDir.resolve("surround.java") `has content` l"""
-        package com.example;
-        import com.awesome.lib.AwesomeClass;
-        new AwesomeClass().doAmazingStuff(); // Wunderbar !
-        // this file was written by some team at some date for a precise purpose
-        """
+            outputDir.resolve("one.java") `has content` l"""
+            package com.example;
+            class One { }
+            """
+            outputDir.resolve("two.java") `has content` l"""
+            package com.example;
+            class Two { }
+            """
 
     testWithinYDocContext("Check ok for no snippet and valid markdown"):
         (outputDir, configDir, workingDir) =>
@@ -120,15 +74,15 @@ class YadladocSuite
     ): (outputDir, configDir, workingDir) =>
         val markdownFile = makeFile(workingDir, "README.md"):
             l"""
-            ```java ydoc.example=ok.java
-            println("Hello world");
-            ```
-            """
+                ```java ydoc.example=ok.java
+                println("Hello world");
+                ```
+                """
         makeFile(outputDir, "ok.java"):
             l"""
-            package com.example;
-            println("Hello world");
-            """
+                package com.example;
+                println("Hello world");
+                """
 
         Yadladoc(testConfig(configDir))
             .check(outputDir, markdownFile) matches:
@@ -157,56 +111,6 @@ class YadladocSuite
             .errors `contains exactly in any order` List(
           CheckErrors.MissingFile(p"ko.java")
         )
-
-    testWithinYDocContext("Sanitized example name available in template"):
-        (outputDir, configDir, workingDir) =>
-            val markdownFile = makeFile(workingDir, "README.md"):
-                l"""
-            ```txt ydoc.example=a/b.txt
-            Test
-            ```
-            """
-            makeFile(
-              configDir / "includes",
-              "txt.template",
-              "${{ydoc.exampleName}}"
-            )
-            Yadladoc(testConfig(configDir))
-                .run(outputDir, markdownFile)
-
-            outputDir.resolve("a/b.txt") `has content` "a_b_txt"
-
-    testWithinYDocContext("Indexed example part name available in template"):
-        (outputDir, configDir, workingDir) =>
-            val markdownFile = makeFile(workingDir, "README.md"):
-                l"""
-            ```java ydoc.example=javaExample.java ydoc.prefix=A
-            class A {}
-            ```
-            ```java ydoc.example=javaExample.java ydoc.prefix=B
-            class B {}
-            ```
-            """
-            makeFile(
-              configDir / "includes",
-              "A.template",
-              "// ${{ydoc.subExampleName}}"
-            )
-            makeFile(
-              configDir / "includes",
-              "B.template",
-              "// ${{ydoc.subExampleName}}"
-            )
-            Yadladoc(testConfig(configDir))
-                .run(outputDir, markdownFile)
-
-            outputDir.resolve("javaExample.java") `has content` l"""
-            package com.example;
-            // javaExample_java_0
-            class A {}
-            // javaExample_java_1
-            class B {}
-            """
 
     testWithinYDocContext("Custom template per example"):
         (outputDir, configDir, workingDir) =>
@@ -266,42 +170,17 @@ class YadladocSuite
         val withYdocContext =
             FunFixture.map3(withTempDir, withTempDir, withTempDir)
         withYdocContext.test(name): (outputDir, configDir, workingDir) =>
-            makeFile(configDir / "includes", "kotlin.template")(
-              TestContext.kotlinTemplate
-            )
             makeFile(configDir / "includes", "java.template")(
-              TestContext.javaTemplate
-            )
-            makeFile(configDir / "includes", "import-prefix.template")(
-              TestContext.importPrefixTemplate
-            )
-            makeFile(configDir / "includes", "footnote-suffix.template")(
-              TestContext.footNoteSuffixTemplate
+              javaTemplate
             )
             f(outputDir, configDir, workingDir)
 
     private def testConfig(configDir: Path): Configuration =
         ConfigurationFromFile(configDir, ConfigurationConstants.DEFAULTS)
 
-    private object TestContext:
-        val kotlinTemplate = l"""
-            package com.example
-            fun main() {
-            $${{ydoc.snippet}}
-            }
-            """
-
-        val javaTemplate = l"""
-            package com.example;
-            $${{ydoc.snippet}}
-            """
-
-        val importPrefixTemplate = l"""
-            import com.awesome.lib.AwesomeClass;
-        """
-
-        val footNoteSuffixTemplate = l"""
-        // this file was written by some team at some date for a precise purpose
+    private val javaTemplate = l"""
+        package com.example;
+        $${{ydoc.snippet}}
         """
 
 end YadladocSuite
