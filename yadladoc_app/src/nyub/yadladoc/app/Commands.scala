ba642cc39bd0a814f236d77e3eb2eca51b0308df
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-17 17:52:47 +0200

    Use shared printer for all print operations to anticipate further printing control

--- yadladoc_app/src/nyub/yadladoc/app/Commands.scala
+++ yadladoc_app/src/nyub/yadladoc/app/Commands.scala
@@ -22,20 +22,20 @@ case class YdocMain(printer: AnsiPrinter = AnsiPrinter.NO_COLOR)
     override def parse(args: Seq[String]): Command = args match
         case "check" :: rest => Check(Seq.empty, printer = printer).parse(rest)
         case "run" :: rest   => Run(Seq.empty, printer = printer).parse(rest)
-        case "help" :: rest  => Help(false).parse(rest)
+        case "help" :: rest  => Help(false, printer = printer).parse(rest)
         case "--color" :: rest =>
             this.copy(printer = AnsiPrinter.WITH_COLOR).parse(rest)
         case Nil => this
-        case _   => Help(true)
+        case _   => Help(true, printer = printer)
 
-    override def run(): ExitCode = Help(true).run()
+    override def run(): ExitCode = Help(true, printer = printer).run()
 
-class Help(failureMode: Boolean) extends Command:
+class Help(failureMode: Boolean, printer: AnsiPrinter) extends Command:
     override def parse(args: Seq[String]): Command =
-        if args.isEmpty then this else Help(true)
+        if args.isEmpty then this else Help(true, printer = printer)
 
     override def run(): ExitCode =
-        println(help)
+        printer.println(help)
         if failureMode then INVALID_ARGUMENT_RETURN_CODE else OK_RETURN_CODE
 
     private val help = """Usage: yadladoc {run|check} [markdown_files, ...]
@@ -44,40 +44,41 @@ class Help(failureMode: Boolean) extends Command:
 Common options:
     --color: use color when printing errors or diffs to the console"""
 
-case class Check(files: Seq[Path], printer: AnsiPrinter = AnsiPrinter.NO_COLOR)
-    extends Command:
+case class Check(files: Seq[Path], printer: AnsiPrinter) extends Command:
     override def parse(args: Seq[String]): Command =
-        if files.isEmpty && args.isEmpty then Help(true)
+        if files.isEmpty && args.isEmpty then Help(true, printer)
         else this.copy(files = files ++ args.map(Paths.get(_)))
 
     override def run(): ExitCode =
         val outputDir = Paths.get(".")
-        withYadladoc: yadladoc =>
+        withYadladoc(printer): yadladoc =>
             val (checked, errors) =
                 applyYadladocOnEachFile(files, yadladoc.check(outputDir, _))
             checked.foreach: g =>
-                println(s"Checked ${g.short} generated from ${g.from}")
+                printer.println(s"Checked ${g.short} generated from ${g.from}")
             printErrorsThenExitCode(errors, printer)
 
 case class Run(files: Seq[Path], printer: AnsiPrinter = AnsiPrinter.NO_COLOR)
     extends Command:
     override def parse(args: Seq[String]): Command =
-        if files.isEmpty && args.isEmpty then Help(true)
+        if files.isEmpty && args.isEmpty then Help(true, printer)
         else this.copy(files = files ++ args.map(Paths.get(_)))
 
     override def run(): ExitCode =
         val outputDir = Paths.get(".")
-        withYadladoc: yadladoc =>
+        withYadladoc(printer): yadladoc =>
             val (generated, errors) =
                 applyYadladocOnEachFile(files, yadladoc.run(outputDir, _))
             generated.foreach: g =>
-                println(s"Generated ${g.short} from ${g.from}")
+                printer.println(s"Generated ${g.short} from ${g.from}")
             printErrorsThenExitCode(errors, printer)
 
-private def withYadladoc(operation: Yadladoc => ExitCode): ExitCode =
+private def withYadladoc(printer: AnsiPrinter)(
+    operation: Yadladoc => ExitCode
+): ExitCode =
     val configDir = Paths.get(".ydoc")
     if !configDir.toFile().isDirectory() then
-        println(".ydoc is not a valid directory")
+        printer.println(".ydoc is not a valid directory")
         INVALID_ARGUMENT_RETURN_CODE
     else
         val yadladoc = Yadladoc(
@@ -98,8 +99,12 @@ private def printErrorsThenExitCode(
     printer: AnsiPrinter
 ): ExitCode =
     documentationErrors.foreach: e =>
-        println(
-          s"Error [${e.getClass().getSimpleName()}]: ${printer.print(e.prettyPrintedMessage)}"
+        printer.println(
+          s"Error [${e.getClass().getSimpleName()}]: ${e.prettyPrintedMessage}"
         )
     if !documentationErrors.isEmpty then DOCUMENTATION_PROBLEM_RETURN_CODE
     else OK_RETURN_CODE
+
+extension (printer: AnsiPrinter)
+    def println(any: Any): Unit =
+        scala.Predef.println(printer.print(any.toString()))
