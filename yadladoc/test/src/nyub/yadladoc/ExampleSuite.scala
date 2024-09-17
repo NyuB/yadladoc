276b5625693e909894341e1ecbac04f4e84130aa
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-17 09:41:53 +0200

    Some YadadocSuite tests actually test Example, report them

---
+++ yadladoc/test/src/nyub/yadladoc/ExampleSuite.scala
@@ -0,0 +1,87 @@
+package nyub.yadladoc
+
+import nyub.assert.AssertExtensions
+import nyub.templating.SurroundingTemplateInjection
+
+class ExampleSuite extends munit.FunSuite with AssertExtensions:
+    test("Sanitized example name available in main template"):
+        Example(
+          "a/b.txt",
+          templateId,
+          None,
+          Seq(
+            bodyOnlyExampleContent("Line #1")
+          )
+        ).build(
+          templateInjection,
+          getTemplate,
+          bodyInjectionKey,
+          exampleNameInjectionKey,
+          partNameInjectionKey,
+          Map.empty
+        ) `is equal to`
+            Seq("a_b_txt", "Line #1")
+
+    test("Sanitized & indexed example name available in prefix template"):
+        val contentWithPrefix =
+            ExampleContent(Seq(prefixTemplateId), Seq("Body"), Seq.empty)
+        val example = Example("a.txt", templateId, None, Seq(contentWithPrefix))
+        example.build(
+          templateInjection,
+          getTemplate,
+          bodyInjectionKey,
+          exampleNameInjectionKey,
+          partNameInjectionKey,
+          Map.empty
+        ) `is equal to` Seq(
+          "a_txt",
+          "a_txt_0\nBody"
+        )
+
+    test("Sanitized & indexed example name available in suffix template"):
+        val contentWithPrefix =
+            ExampleContent(Seq.empty, Seq("Body"), Seq(suffixTemplateId))
+        val example = Example("a.txt", templateId, None, Seq(contentWithPrefix))
+        example.build(
+          templateInjection,
+          getTemplate,
+          bodyInjectionKey,
+          exampleNameInjectionKey,
+          partNameInjectionKey,
+          Map.empty
+        ) `is equal to` Seq(
+          "a_txt",
+          "Body\na_txt_0"
+        )
+
+    private val exampleNameInjectionKey = "exampleNameInjectionKey"
+    private val partNameInjectionKey = "partNameInjectionKey"
+    private val templateInjection = SurroundingTemplateInjection("<<", ">>")
+    private val bodyInjectionKey = "bodyInjectionKey"
+    private val testTemplate =
+        Seq(
+          s"<<$exampleNameInjectionKey>>",
+          s"<<$bodyInjectionKey>>"
+        )
+
+    private val testPrefixTemplate =
+        Seq(
+          s"<<$partNameInjectionKey>>"
+        )
+
+    private val testSuffixTemplate = testPrefixTemplate
+
+    private val templateId = TemplateId("templateId")
+    private val prefixTemplateId = TemplateId("prefixId")
+    private val suffixTemplateId = TemplateId("suffixId")
+    private def getTemplate(id: TemplateId): Iterable[String] =
+        if id == templateId then testTemplate
+        else if id == suffixTemplateId then testSuffixTemplate
+        else if id == prefixTemplateId then testPrefixTemplate
+        else throw IllegalStateException(s"Unknown template id $id")
+
+    private def bodyOnlyExampleContent(
+        lines: String*
+    ): ExampleContent = ExampleContent(Seq.empty, lines, Seq.empty)
+
+end ExampleSuite
