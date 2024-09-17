7d7939613ccffe8578afd817eb095c30a038f714
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-09-17 06:19:45 +0200

    Split documentation kind function for each case

--- yadladoc/test/src/nyub/yadladoc/ConfigurationSuite.scala
+++ yadladoc/test/src/nyub/yadladoc/ConfigurationSuite.scala
@@ -6,7 +6,30 @@ import nyub.interpreter.CramDecoratorService
 import nyub.interpreter.JShellDecoratorService
 
 class ConfigurationSuite extends munit.FunSuite with AssertExtensions:
-    test("built-in services"):
+    test("Documentation kind is example when ydoc.example property is present"):
+        TestConfig.documentationKindForSnippet(
+          Snippet(None, Seq.empty, Properties("ydoc.example" -> "A"))
+        ) matches:
+            case DocumentationKind.ExampleSnippet("A", _) => ()
+
+    test(
+      "Documentation kind is decorated when ydoc.decorator property is present"
+    ):
+        TestConfig.documentationKindForSnippet(
+          Snippet(
+            None,
+            Seq.empty,
+            Properties("ydoc.decorator" -> "decoratorId")
+          )
+        ) `is equal to`
+            DocumentationKind.DecoratedSnippet("decoratorId")
+
+    test("Documentation kind is raw when no special property is present"):
+        TestConfig.documentationKindForSnippet(
+          Snippet(None, Seq.empty, Properties())
+        ) `is equal to` DocumentationKind.Raw
+
+    test("Built-in services"):
         TestConfig.decoratorServices
             .get("cram")
             .map(_.getClass()) `is equal to some` classOf[CramDecoratorService]
