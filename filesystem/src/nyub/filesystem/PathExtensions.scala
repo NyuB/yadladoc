13068ad933e59a9603ca84335c78379955badc12
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-01-29 19:55:01 +0100

    Fix ignored trailing new line

--- filesystem/src/nyub/filesystem/PathExtensions.scala
+++ filesystem/src/nyub/filesystem/PathExtensions.scala
@@ -7,6 +7,19 @@ extension (p: Path)
     def /(other: Path) = p.resolve(other)
     def /(other: String) = p.resolve(other)
     def lines: Iterable[String] =
-        val res = scala.collection.mutable.ArrayBuffer[String]()
-        Files.lines(p).forEach(res.addOne)
-        res
+        val all = Files.readString(p).splitWithDelimiters("(\r\n)|(\n)", 0)
+        DelimiterIterator(all).to(List)
+
+private class DelimiterIterator(private val delimited: Array[String])
+    extends Iterator[String]:
+    private var i = 0
+    private var addOneEmptyLine = delimited.length % 2 == 0
+    override def hasNext: Boolean = addOneEmptyLine || i < delimited.length
+    override def next(): String =
+        if i >= delimited.length && addOneEmptyLine then
+            addOneEmptyLine = false
+            ""
+        else
+            val res = delimited(i)
+            i += 2
+            res
--- filesystem/test/src/nyub/filesystem/PathExtensionsSuite.scala
+++ filesystem/test/src/nyub/filesystem/PathExtensionsSuite.scala
@@ -7,9 +7,14 @@ class PathExtensionsSuite extends munit.FunSuite with AssertExtensions:
     test("Read lines from path"):
         val tmp = Files.createTempDirectory("testLines") / "content.txt"
         Files.writeString(tmp, "First line\nSecond line")
-        tmp.lines `is equal to` List("First line", "Second line")
+        tmp.lines `is equal to` Iterable("First line", "Second line")
 
     test("Return an empty string as last line for trailing newline"):
         val tmp = Files.createTempDirectory("testLines") / "content.txt"
         Files.writeString(tmp, "First line\n")
-        tmp.lines `is equal to` List("First line", "")
+        tmp.lines `is equal to` Iterable("First line", "")
+
+    test("Handle \\r\\n as single newline"):
+        val tmp = Files.createTempDirectory("testLines") / "content.txt"
+        Files.writeString(tmp, "A\r\nB")
+        tmp.lines `is equal to` Iterable("A", "B")
