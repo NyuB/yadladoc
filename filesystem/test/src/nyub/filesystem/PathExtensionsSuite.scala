232ce633b87d429e07fc81fbfe6125fcea148fc7
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-01-29 19:00:26 +0100

    Exhibit problem with trailing newline

--- filesystem/test/src/nyub/filesystem/PathExtensionsSuite.scala
+++ filesystem/test/src/nyub/filesystem/PathExtensionsSuite.scala
@@ -8,3 +8,8 @@ class PathExtensionsSuite extends munit.FunSuite with AssertExtensions:
         val tmp = Files.createTempDirectory("testLines") / "content.txt"
         Files.writeString(tmp, "First line\nSecond line")
         tmp.lines `is equal to` List("First line", "Second line")
+
+    test("Return an empty string as last line for trailing newline"):
+        val tmp = Files.createTempDirectory("testLines") / "content.txt"
+        Files.writeString(tmp, "First line\n")
+        tmp.lines `is equal to` List("First line", "")
